9e0cd73de060894f153f2e3cdd9c7d33a34a6f7f
Papa Dieng <Alassanedieng004@gmail.com>
2025-10-23 13:15:35 -0400

    Added Spring Boot project files

---
+++ src/main/java/edu/uncg/giraffegallery/giraffe_api/GiraffeApiApplication.java
@@ -0,0 +1,13 @@
+package edu.uncg.giraffegallery.giraffe_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiraffeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiraffeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uncg/giraffegallery/giraffe_api/GiraffeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uncg.giraffegallery.giraffe_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraffeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
